70bd0bccbf25c3342cf48dee9f2ef177cd0335c9
Muhsin Ünsal <zorkon2002@gmail.com>
2021-12-12 15:54:47 +0300

    Main template

---
+++ script.js
@@ -0,0 +1,14 @@
+const MATH101 = {
+    "proporites" : {
+        "Quiz": 7,
+        "Midterm": 2,
+        "Kalmasiniri": "Unknown",
+    },
+    "grading_ratios":{
+        "MidtermI": 30,
+        "MidtermII": 30,
+        "Final": 40,
+        "Quiz": 10
+    }    
+}
+const courses = [MATH101];
