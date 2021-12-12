578a90b685b7ed3a56fe4cf385eca838c52fb480
Muhsin Ünsal <zorkon2002@gmail.com>
2021-12-12 19:02:56 +0300

    1.0

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const MATH151 = {
     Kalmasiniri: "Unknown",
   },
   grading_ratios: {
-    MidtermI: 30,
-    MidtermII: 30,
+    Midterm_1: 30,
+    Midterm_2: 30,
     Final: 40,
     Quiz: 10,
   },
@@ -22,10 +22,11 @@ const CHE105 = {
     Kalmasiniri: 50,
   },
   grading_ratios: {
-    MidtermI: 25,
-    MidtermII: 25,
+    Midterm_1: 25,
+    Midterm_2: 25,
     Final: 35,
     Laboratory: 15,
+    Assignment: 10,
   },
 };
 const PHYS101 = {
@@ -37,8 +38,8 @@ const PHYS101 = {
     Kalmasiniri: "Unknown",
   },
   grading_ratios: {
-    MidtermI: 20,
-    MidtermII: 20,
+    Midterm_1: 20,
+    Midterm_2: 20,
     Final: 25,
     Laboratory: 20,
     Assignment: 15,
@@ -47,26 +48,24 @@ const PHYS101 = {
 
 const courses = [MATH151, CHE105, PHYS101];
 
-function whatCourse(string){
-    let course
-    switch (string) {
-      case "MATH 151":
-        course = MATH151;
-        break;
-      case "CHE 105":
-        course = CHE105;
-        break;
-      case "PHYS 101":
-        course = PHYS101;
-        break;
-      default:
-          console.error("Whatcourse functionuna yanlış string girildi!");
-        break;
-    }
-    return course
+function whatCourse(string) {
+  let course;
+  switch (string) {
+    case "MATH 151":
+      course = MATH151;
+      break;
+    case "CHE 105":
+      course = CHE105;
+      break;
+    case "PHYS 101":
+      course = PHYS101;
+      break;
+    default:
+      console.error("Whatcourse functionuna yanlış string girildi!");
+      break;
+  }
+  return course;
 }
-
-
 
 for (let i = 0; i < courses.length; i++) {
   const course = courses[i];
@@ -76,20 +75,51 @@ for (let i = 0; i < courses.length; i++) {
   document.getElementById("courses").appendChild(option);
 }
 
-
 const course_i = document.getElementById("course");
 
 const table = document.createElement("table");
-const row1 = document.createElement("td");
-const row2 = document.createElement("td");
+const row1 = document.createElement("tr");
+const row2 = document.createElement("tr");
 table.appendChild(row1);
 table.appendChild(row2);
+let result = 0;
 
-course_i.addEventListener("change", ()=>{
-    const courseobj = whatCourse(course_i.value);
-    console.log(courseobj)
+course_i.addEventListener("change", () => {
+    result = 0;
+    document.getElementById("totalgrade").innerHTML = `Your grade is: ${result}`;
+    row1.innerHTML = "";
+    row2.innerHTML = "";
+     
+
+  const courseobj = whatCourse(course_i.value);
+  for (let i = 0; i < Object.keys(courseobj.grading_ratios).length; i++) {
+    let property = Object.keys(courseobj.grading_ratios)[i];
+
+    let td1 = document.createElement("td");
+    td1.setAttribute("id", property);
+    td1.innerHTML = `${property} (${courseobj.grading_ratios[property]}%)`;
+    row1.appendChild(td1);
+
+    let td2 = document.createElement("td");
+    td2.setAttribute("id", `${property}_i`);
+    let input = document.createElement("input");
+    input.setAttribute("type", "number");
+    input.setAttribute("id", `${property}_i`);
+    input.addEventListener("change", () => {
+      result += (input.value * courseobj.grading_ratios[property]) / 100;
+      document.getElementById("totalgrade").innerHTML = `Your grade is: ${result}`
+    });
+    td2.appendChild(input);
+    row2.appendChild(td2);
+  }
 });
+
 
 document.getElementById("s2").appendChild(table);
 
 
+let Hmm = document.createElement("div");
+let quote = ["Made with boredom.","Why ?","🙂","🤣","😐","Made with love"];
+Hmm.setAttribute("id","Hmm");
+Hmm.innerHTML = quote[Math.floor(Math.random() * quote.length)];
+document.getElementById("fondation").appendChild(Hmm);
